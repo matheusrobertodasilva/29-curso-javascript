2a5bbfa95d8c393f115423f23e165565f8c383b9
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2022-06-01 15:52:41 -0300

    aula 3, assuntos: sobre o js; sobre o nodejs; erros; console

---
+++ aula3/erros.js
@@ -0,0 +1,16 @@
+//const numero; /erro de syntax nesse caso 
+const numero = 1;
+//const minhaVar = "oi";
+//console.log(numero //erro de syntax pois falta o fechamento do parenteses 
+console.log(minhaVar) // erro de referencia pois a variavel entre parenteses não foi declarada 
+
+
+
+
+/* 
+tipos de erro 
+
+SyntaxError;
+ReferenceError; 
+
+*/
---
+++ aula3/oJavascript.js
@@ -0,0 +1,10 @@
+// tipagem dinâmica 
+// javascript é um tipo untyped => não precisa declarar o tipo de dado
+// javascript é uma linguagem multparadigma isso que dizer que um problema pode ser resolvido de diversas formas 
+// Linguagem interpretada:  
+
+let minhaVar = 876;
+minhaVar = "texto";
+minhaVar = true;
+
+// Nodejs: É um interpretador da liguagem fora do navegador, isso faz com que possamos usar o o js fora do navegador, nesse caso o back-end. Ele é a ferramenta que usamos para rodar o js fora do navegado e tem suas diferenças. 
